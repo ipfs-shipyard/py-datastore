0b35582f8285ad6b7f41daac3f48a361fb15c737
Aliabbas Merchant <merchantaliabbas@gmail.com>
2019-08-05 09:23:30 +0530

    Added Config etc files | Build will fail

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
 import re
 from setuptools import setup, find_packages
 
@@ -8,38 +8,28 @@ pkgname = 'datastore'
 # gather the package information
 main_py = open('datastore/core/__init__.py').read()
 metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", main_py))
-packages = filter(lambda p: p.startswith(pkgname), find_packages())
-
-# convert the readme to pypi compatible rst
-try:
-  try:
-    import pypandoc
-    readme = pypandoc.convert('README.md', 'rst')
-  except ImportError:
-    readme = open('README.md').read()
-except:
-  print 'something went wrong reading the README.md file.'
-  readme = ''
 
 setup(
   name=pkgname,
   version=metadata['version'],
   description='simple, unified API for multiple data stores',
-  long_description=readme,
+  long_description_markdown_filename='README.md',
   author=metadata['author'],
   author_email=metadata['email'],
-  url='http://github.com/datastore/datastore',
+  url='http://github.com/ipfs/py-datastore',
   keywords=[
     'datastore',
     'unified api',
     'database',
   ],
-  packages=packages,
+  packages=find_packages(include='datastore'),
   namespace_packages=['datastore'],
-  test_suite='datastore.test',
+  # test_suite='datastore.test',
   license='MIT License',
   classifiers=[
     'Topic :: Database',
     'Topic :: Database :: Front-Ends',
-  ]
+  ],
+  include_package_data=True,
+  py_modules=['datastore'],
 )
