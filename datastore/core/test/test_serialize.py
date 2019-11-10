4827725973830761c84914ad8b29d9e627df9b20
Alexander Schlarb <alexander@ninetailed.ninja>
2019-11-10 23:13:33 +0100

    Split `serialize.py` into several backends and fix tests

--- datastore/core/test/test_serialize.py
+++ datastore/core/test/test_serialize.py
@@ -1,12 +1,9 @@
-import pickle
-import unittest
-
 import pytest
+import trio.testing
 
-from datastore.core.key import Key
-from datastore.core.basic import DictDatastore
-from datastore.core.serialize import *
-from datastore.core.test.test_basic import TestDatastore
+import datastore
+import datastore.serializer.json
+import datastore.serializer.pickle
 
 
 def implements_serializer_interface(cls):
@@ -14,123 +11,109 @@ def implements_serializer_interface(cls):
 	       and hasattr(cls, 'dumps') and callable(cls.dumps)
 
 
-class TestSerialize(TestDatastore):
+def test_basic(DatastoreTests):
+	serializer = datastore.serializer.json.Serializer()
+	
+	values_orig = [{"value": i} for i in range(0, 1000)]
+	values_json = [serializer.dumps([item]) for item in values_orig]
+	
+	# test generators
+	values_serialized = [serializer.dumps([item]) for item in values_orig]
+	values_deserialized = [serializer.loads(item)[0] for item in values_serialized]
+	assert values_serialized == values_json
+	assert values_deserialized == values_orig
 
-	def test_basic(self):
-		value = 'test_value_%s' % self
-		values_raw = [{'value': i} for i in range(0, 1000)]
-		values_json = map(json.dumps, values_raw)
 
-		# test protocol
-		with pytest.raises(NotImplementedError):
-		    Serializer.loads(value)
-		with pytest.raises(NotImplementedError):
-		    Serializer.dumps(value)
+async def subtest_serializer_shim(serializer=None, numelems=100):
+	child = datastore.BinaryDictDatastore()
+	shim = datastore.SerializerAdapter(child, serializer=serializer)
+	
+	values_raw = [{"value": i} for i in range(0, numelems)]
+	
+	values_serial = [serializer.dumps([v]) for v in values_raw]
+	values_deserial = [serializer.loads(v)[0] for v in values_serial]
+	assert values_deserial == values_raw
+	
+	for value in values_raw:
+		key = datastore.Key(value["value"])
+		value_serialized = serializer.dumps([value])
+		
+		# should not be there yet
+		assert not await shim.contains(key)
+		with pytest.raises(KeyError):
+			assert await shim.get_all(key)
+		
+		# put (should be there)
+		await shim.put(key, [value])
+		assert await shim.contains(key)
+		assert await shim.get_all(key) == [value]
+		
+		# make sure underlying DictDatastore is storing the serialized value.
+		assert await shim.child_datastore.get_all(key) == value_serialized
+		
+		# delete (should not be there afterwards)
+		await shim.delete(key)
+		assert not await shim.contains(key)
+		with pytest.raises(KeyError):
+			await shim.get_all(key)
+		
+		# make sure manipulating underlying DictDatastore works equally well.
+		await shim.child_datastore.put(key, value_serialized)
+		assert await shim.contains(key)
+		assert await shim.get_all(key) == [value]
+		
+		await shim.child_datastore.delete(key)
+		assert not await shim.contains(key)
+		with pytest.raises(KeyError):
+			await shim.get_all(key)
 
-		# test non serializer
-		assert NonSerializer.loads(value) == value
-		assert NonSerializer.dumps(value) == value
-		assert NonSerializer.loads(value) is value
-		assert NonSerializer.dumps(value) is value
 
-		# test generators
-		values_serialized = list(serialized_gen(json, values_raw))
-		values_deserialized = list(deserialized_gen(json, values_serialized))
-		assert values_serialized == values_json
-		assert values_deserialized == values_raw
+@trio.testing.trio_test
+async def test_serializer_shim():
+	#XXX: Testing directly with the `json` module is not going to work as that
+	#     module expects input strings to be text, rather then binary
+	await subtest_serializer_shim(datastore.serializer.json.Serializer())
+	await subtest_serializer_shim(datastore.serializer.json.PrettySerializer())
+	await subtest_serializer_shim(datastore.serializer.pickle.Serializer())
 
-		# test stack
-		stack = Stack([json, MapSerializer])
-		values_serialized = map(stack.dumps, values_raw)
-		values_deserialized = map(stack.loads, values_serialized)
-		assert values_deserialized == values_raw
 
-	def subtest_serializer_shim(self, serializer, numelems=100):
-		child = DictDatastore()
-		shim = SerializerShimDatastore(child, serializer=serializer)
+def test_interface_check_returns_true_for_valid_serializers():
+	class S(object):
+		def loads(self, foo):
+			return foo
 
-		values_raw = [{'value': i} for i in range(0, numelems)]
+		def dumps(self, foo):
+			return foo
 
-		values_serial = [serializer.dumps(v) for v in values_raw]
-		values_deserial = [serializer.loads(v) for v in values_serial]
-		assert values_deserial == values_raw
+	assert implements_serializer_interface(S)
+	assert implements_serializer_interface(datastore.serializer.json.Serializer())
+	assert implements_serializer_interface(datastore.serializer.json.PrettySerializer())
+	assert implements_serializer_interface(datastore.serializer.pickle.Serializer())
+	assert implements_serializer_interface(datastore.abc.Serializer)
 
-		for value in values_raw:
-			key = Key(value['value'])
-			value_serialized = serializer.dumps(value)
 
-			# should not be there yet
-			assert not shim.contains(key)
-			assert shim.get(key) == None
+def test_interface_check_returns_false_for_invalid_serializers():
+	class S1(object):
+		pass
 
-			# put (should be there)
-			shim.put(key, value)
-			assert shim.contains(key)
-			assert shim.get(key) == value
+	class S2(object):
+		def loads(self, foo):
+			return foo
 
-			# make sure underlying DictDatastore is storing the serialized value.
-			assert shim.child_datastore.get(key) == value_serialized
+	class S3(object):
+		def dumps(self, foo):
+			return foo
 
-			# delete (should not be there)
-			shim.delete(key)
-			assert not shim.contains(key)
-			assert shim.get(key) == None
+	class S4(object):
+		def dumps(self, foo):
+			return foo
 
-			# make sure manipulating underlying DictDatastore works equally well.
-			shim.child_datastore.put(key, value_serialized)
-			assert shim.contains(key)
-			assert shim.get(key) == value
+	class S5(object):
+		loads = 'loads'
+		dumps = 'dumps'
 
-			shim.child_datastore.delete(key)
-			assert not shim.contains(key)
-			assert shim.get(key) == None
-
-	def test_serializer_shim(self):
-		self.subtest_serializer_shim(json)
-		self.subtest_serializer_shim(PrettyJSON)
-		self.subtest_serializer_shim(pickle)
-		self.subtest_serializer_shim(MapSerializer)
-		self.subtest_serializer_shim(default_serializer)  # module default
-
-		self.subtest_serializer_shim(Stack([MapSerializer]))
-		self.subtest_serializer_shim(Stack([json, MapSerializer]))
-		self.subtest_serializer_shim(Stack([json, MapSerializer, pickle]))
-
-	def test_interface_check_returns_true_for_valid_serializers(self):
-		class S(object):
-			def loads(self, foo):
-				return foo
-
-			def dumps(self, foo):
-				return foo
-
-		assert implements_serializer_interface(S)
-		assert implements_serializer_interface(json)
-		assert implements_serializer_interface(pickle)
-		assert implements_serializer_interface(Serializer)
-
-	def test_interface_check_returns_false_for_invalid_serializers(self):
-		class S1(object):
-			pass
-
-		class S2(object):
-			def loads(self, foo):
-				return foo
-
-		class S3(object):
-			def dumps(self, foo):
-				return foo
-
-		class S4(object):
-			def dumps(self, foo):
-				return foo
-
-		class S5(object):
-			loads = 'loads'
-			dumps = 'dumps'
-
-		assert not implements_serializer_interface(S1)
-		assert not implements_serializer_interface(S2)
-		assert not implements_serializer_interface(S3)
-		assert not implements_serializer_interface(S4)
-		assert not implements_serializer_interface(S5)
+	assert not implements_serializer_interface(S1)
+	assert not implements_serializer_interface(S2)
+	assert not implements_serializer_interface(S3)
+	assert not implements_serializer_interface(S4)
+	assert not implements_serializer_interface(S5)
---
+++ datastore/serializer/json.py
@@ -0,0 +1,59 @@
+import json
+import typing
+
+import datastore.abc
+
+
+T_co = typing.TypeVar("T_co", covariant=True)
+
+
+#FIXME: This stuff should support streaming data to the maximum extent possible
+
+
+class Serializer(datastore.abc.Serializer[T_co], typing.Generic[T_co]):
+	"""json wrapper serializer that gives the most compact representation possible.
+	"""
+	
+	__slots__ = ("encoding", "indent", "separators")
+	
+	encoding:   str
+	indent:     typing.Optional[typing.Union[int, str]]
+	separators: typing.Tuple[str, str]
+	
+	
+	def __init__(self, *, encoding: str = "utf-8",
+	             indent: typing.Optional[typing.Union[int, str]] = None,
+	             separators: typing.Tuple[str, str] = (',', ':')):
+		self.encoding   = encoding
+		self.indent     = indent
+		self.separators = separators
+	
+	
+	def loads(self, value: bytes) -> typing.List[T_co]:
+		"""returns json deserialized `value`."""
+		return [json.loads(value)]  #FIXME: Broken if more than one object
+	
+	
+	def dumps(self, value: typing.Iterable[T_co]) -> bytes:
+		"""returns json serialized `value` (pretty-printed)."""
+		result = bytearray()
+		for item in value:
+			# We force `ensure_ascii=False` here to most compact encoding of
+			# having non-ascii characters be encoded by the given encoding
+			# rather then using JSON string "\uXXXX" notation.
+			# We also force `sort_keys=True` to ensure the output is
+			# reproducible between interpreter runs.
+			result += json.dumps(item, ensure_ascii=False, sort_keys=True,
+			                     indent=self.indent, separators=self.separators
+			).encode(self.encoding)
+		return bytes(result)
+
+
+
+class PrettySerializer(Serializer[T_co], typing.Generic[T_co]):
+	"""json wrapper serializer that pretty-prints.
+	Useful for human readable values and versioning.
+	"""
+	
+	def __init__(self, *, encoding: str = "utf-8"):
+		super().__init__(encoding=encoding, indent="\t", separators=(", ", ": "))
