32a6d3a7d8d5f8057473e83f0c09d31fa71e9b85
Adam Sawicki <adam.sawicki@ibm.com>
2020-02-20 00:25:32 -0500

    Update FlatFSAdapter to match Adapter spec

--- datastore/adapter/keytransform.py
+++ datastore/adapter/keytransform.py
@@ -1,6 +1,5 @@
 import functools
 import typing
-from pathlib import Path
 
 import datastore
 
@@ -28,6 +27,7 @@ __all__ = [
 
 
 KEY_TRANSFORM_T = typing.Callable[[datastore.Key], datastore.Key]
+T = typing.TypeVar('T', bound="_Adapter")
 
 
 class _Adapter(typing.Generic[DS]):
@@ -102,7 +102,6 @@ class ObjectAdapter(_Adapter[datastore.abc.ObjectDatastore], datastore.abc.Objec
 	...
 
 
-
 class _LowercaseKeyAdapter(_Adapter[DS], typing.Generic[DS]):
 	"""Represents a simple DatastoreAdapter that lowercases all incoming keys.
 	
@@ -143,7 +142,6 @@ class BinaryLowercaseKeyAdapter(_LowercaseKeyAdapter[datastore.abc.BinaryDatasto
 
 class ObjectLowercaseKeyAdapter(_LowercaseKeyAdapter[datastore.abc.ObjectDatastore], datastore.abc.ObjectAdapter):  # noqa: E501
 	...
-
 
 
 class _NamespaceAdapter(_Adapter[DS], typing.Generic[DS]):
@@ -302,40 +300,60 @@ class _FlatFSAdapter(_Adapter[DS], typing.Generic[DS]):
 
 	Incoming keys are sharded into namespaces according to specifications
 	in a `SHARDING` file, which `prefix`, `suffix`, and `next-to-last`
-	sharding functions available.
-
-	The file should be be a single line in the following format:
-		{prefix}/version/function_name/key_length
+	sharding functions available. For example:
 	
 	Implementation Notes:
 		* The `default_prefix` is "/repo/flatfs/shard/"
 		* The only version supported is "v1"
 		* function_name must be `prefix`, `suffix`, or `next-to-last`
 
-	For example:
-
-		>>> import datastore
-		>>>
-		>>> ds = datastore.DictDatastore()
-		>>> np = datastore.FlatFSDatastore(ds)
-		>>>
-		>>> np.put(datastore.Key('/abcdefghijk'), 1)
-		>>> np.get(datastore.Key('/abcdefghijk'))
-		1
-		>>> ds.get(datastore.Key('/abcdefghijk'))
-		None
-		>>> ds.get(datastore.Key('/ab/cd/ef/abcdefghijk'))
-		1
-		>>> np.put(datastore.Key('abc'), 2)
-		>>> np.get(datastore.Key('abc'))
-		2
-		>>> ds.get(datastore.Key('/ab/ca/bc/abc'))
-		2
 	"""
 
-	_default_sharding_file: str = "SHARDING"
-	_default_prefix: str = "/repo/flatfs/shard/"
-	
+	prefix: str = "/repo/flatfs/shard/"
+	_default_sharding_key: datastore.Key = datastore.Key("SHARDING")
+	_default_sharding_func: str = "v1/prefix/2"
+
+	@classmethod
+	async def create(
+			cls: typing.Type[T],
+			child_datastore: DS,
+			*args,
+			sharding_key: datastore.Key = None,
+			default_sharding_func: str = None,
+			**kwargs
+	) -> T:
+		"""Initializes _FlatFSAdapter with sharding function.
+
+		Arguments
+		---------
+		cls
+			Class variable passed in due to `classmethod`
+		child_datastore
+			Underlying datastore always used to initialize an `Adapter`
+		*args
+			Variable length list argument to be passed to cls constructor
+		sharding_key
+			Key used to access the sharding function in the underlying Datastore
+		default_sharding_func
+			Default sharding func to use if one is not found in the datastore
+		"""
+		# Get the variables needed to parse the sharding function
+		sharding_key = (
+			sharding_key
+			if sharding_key is not None
+			else cls._default_sharding_key # type: ignore[attr-defined]
+		)
+		default_sharding_func = (
+			default_sharding_func
+			if default_sharding_func is not None
+			else f"{cls.prefix}{cls._default_sharding_func}" # type: ignore[attr-defined]
+		)
+		sharding_fn: KEY_TRANSFORM_T = await cls._parse_sharding_function( # type: ignore[attr-defined]
+			child_datastore, sharding_key, default_sharding_func
+		)
+		# Return an instantiated version of the class
+		return cls(child_datastore, *args, key_transform=sharding_fn, **kwargs) # type: ignore[attr-defined]
+
 	@staticmethod
 	def _prefix(key: datastore.Key, length: int) -> datastore.Key:
 		return datastore.Key(str(key).ljust(length, "_")[:length])
@@ -348,35 +366,28 @@ class _FlatFSAdapter(_Adapter[DS], typing.Generic[DS]):
 	def _next_to_last(key: datastore.Key, length: int) -> datastore.Key:
 		return datastore.Key(str(key).rjust(length + 1, "_")[:length])
 
-	def __init__(self, *args,
-	             sharding_file: str = None,
-	             prefix: str = None,
-	             **kwargs):
-		"""Initializes _FlatFSAdapter with sharding function.
-
+	@classmethod
+	async def _parse_sharding_function(
+			cls: typing.Type[T],
+			child_datastore: DS,
+			sharding_key: datastore.Key,
+			default_sharding_func: str
+	) -> KEY_TRANSFORM_T:
+		"""Determine the sharding function from the sharding_key.
+		
 		Arguments
 		---------
-		sharding_file:
-			Path to the sharding file
-		prefix:
-			Prefix used in sharding file
-		"""
+		cls
+			Class variable passed in due to `classmethod`
+		child_datastore
+			Underlying datastore always used to access the `sharding_key`
+		sharding_key
+			Key used to access the sharding function in the `child_datastore`
+		default_sharding_func
+			Default sharding func to use if one is not found in `child_datastore`
 
-		# assign the nesting variables
-		self.sharding_file = sharding_file if sharding_file is not None else self._default_sharding_file
-		self.prefix = prefix if prefix is not None else self._default_prefix
-
-		_sharding_function = self._parse_sharding_function()
-		super().__init__(*args, key_transform=_sharding_function, **kwargs)
-
-
-	def _parse_sharding_function(self) -> KEY_TRANSFORM_T:
-		"""Determine the sharding function from the SHARDING file.
-		
 		Raises
 		------
-		Exception
-			Sharding file was not found
 		Exception
 			Empty shard identifier file.
 		Exception
@@ -385,37 +396,44 @@ class _FlatFSAdapter(_Adapter[DS], typing.Generic[DS]):
 			Sharding file was in incorrect format
 		Exception
 			Expect `v1` format
-		Exceptiono
+		Exception
+			`key_length` shouold be an integer
+		Exception
+			`function_name` was not `prefix`, `suffix`, or `next-to-last`
 		RuntimeError
-			An internal error occurred"""
-		# Ensure existence of sharding file
-		path: Path = Path(self.sharding_file)
-		if not path.exists():
-		    raise Exception(f"{self.sharding_file} file does not exist")
+			An internal error occurred
+		"""
 
-		with open(path, "r") as f:
-		    sharding_func: str = f.read()
+		sharding_func: str
+		need_to_store_sharding_func: bool = False
+		try:
+			# Try reading existing sharding func used
+			sharding_func = next(iter(await child_datastore.get_all(sharding_key))) # type: ignore[assignment]
+		except KeyError:
+			# Use the given default sharding func
+			sharding_func = default_sharding_func
+			need_to_store_sharding_func = True
 
 		# Ensure not empty
 		if not sharding_func:
-		    raise Exception("Empty shard identifier file.")
+			raise Exception("Empty shard identifier file.")
 
 		sharding_func = sharding_func.strip()
 
 		# Returns a 3-tuple containing the part before the separator,
 		# the separator itself, and the part after the separator
-		_, _, sharding_func = sharding_func.partition(self.prefix)
+		_, _, sharding_fn = sharding_func.partition(cls.prefix) # type: ignore[attr-defined]
 
 		# Ensure proper format
-		if not sharding_func:
-			raise Exception(f"Prefix ({self.prefix}) was not present in {sharding_func}")
+		if not sharding_fn:
+			raise Exception(f"Prefix ({cls.prefix}) was not present in {sharding_func}") # type: ignore[attr-defined]
 		
-		parts: list = sharding_func.split("/")
+		parts: list = sharding_fn.split("/")
 
 		if len(parts) != 3:
 			raise Exception(
-				f"invalid shard identifier: {sharding_func}.\n"
-				f"Expecting form: {self.prefix}/version/function_name/key_length"
+				f"invalid shard identifier: {sharding_fn}.\n" # type: ignore[attr-defined]
+				f"Expecting form: {cls.prefix}/version/function_name/key_length"
 			)
 
 		version, function_name, length = parts
@@ -428,19 +446,22 @@ class _FlatFSAdapter(_Adapter[DS], typing.Generic[DS]):
 		except ValueError:
 			raise Exception(f"Invalid parameter: {length}. Should be integer representing `key_length`.")
 
-		__funcs: dict = {
-			"prefix": self._prefix,
-			"suffix": self._suffix,
-			"next-to-last": self._next_to_last
+		funcs: dict = {
+			"prefix": cls._prefix, # type: ignore[attr-defined]
+			"suffix": cls._suffix, # type: ignore[attr-defined]
+			"next-to-last": cls._next_to_last # type: ignore[attr-defined]
 		}
 
 		try:
-			_func = __funcs[function_name]
+			func = funcs[function_name]
 		except Exception:
 			raise Exception(f"Expected 'prefix', 'suffix', or 'next-to-last' got: {function_name}")
 
-		return functools.partial(_func, length=length)
+		if need_to_store_sharding_func:
+			# All checks have been passed, put in datastore, remembering it for subsequent use of this repo
+			await child_datastore.put(sharding_key, [sharding_func]) # type: ignore[list-item]
 
+		return functools.partial(func, length=length)
 
 	async def query(self, query: datastore.Query) -> datastore.Cursor:
 		# Requires supporting * operator on queries.
